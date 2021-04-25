7e044b3b771fd1b871e1caebdfde2a9924fcab9a
anlorreen <55151917+anlorreen@users.noreply.github.com>
2021-04-25 22:00:27 +0800

    Added main

---
+++ main.py
@@ -0,0 +1,11 @@
+import engine
+import interface
+
+engine.seedWords()
+interface.printLoading2("game")
+interface.printNameOfGame()
+interface.printInstructions()
+def main():
+	interface.playGame()
+	main()
+main()
